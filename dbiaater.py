bfd1e009e49fbf95bc870e30a022da1528f6de70
Virginia Campos <virginiapscampos@gmail.com>
2022-11-29 22:01:58 -0300

    Commit IA
    Gerador de base de dados e versao inicial de ia

---
+++ dbiaater.py
@@ -0,0 +1,115 @@
+import urllib3
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import re
+import nltk
+import pymysql
+import pandas as pd
+
+    
+def inserePalavraLocalizacao(idperfil, idpalavra, localizacao):
+    conexao = pymysql.connect(host='localhost',user='root',password='vp$C1976',db='iaater', autocommit = True)
+    cursor = conexao.cursor()
+    cursor.execute('insert into palavra_localizacao(idperfil, idpalavra, localizacao) values (%s, %s, %s)', (idperfil, idpalavra, localizacao))
+    idpalavraLocalizacao = cursor.lastrowid
+    cursor.close()
+    return idpalavraLocalizacao
+
+def inserePalavra(palavra):
+    conexao = pymysql.connect(host='localhost',user='root',password='vp$C1976',db='iaater', autocommit = True, use_unicode = True, charset = 'utf8mb4')
+    cursor = conexao.cursor()
+    cursor.execute('insert into palavras(palavra) values (%s)', palavra)
+    idpalavra = cursor.lastrowid
+    cursor.close()
+    conexao.close()
+    return idpalavra
+
+def palavraIndexada(palavra):
+    retorno = -1
+    conexao = pymysql.connect(host='localhost',user='root',password='vp$C1976',db='iaater', autocommit = True, use_unicode = True, charset = 'utf8mb4')
+    cursor = conexao.cursor()
+    cursor.execute('select idpalavra from palavras where palavra = %s', palavra)
+    if cursor.rowcount > 0:
+        # print('Palavra já cadastrada')
+        retorno = cursor.fetchone()[0]
+    # else:
+    #     print('Palavra não cadastrada')
+    cursor.close()
+    conexao.close()
+    return retorno
+
+def inserePerfil(perfil, bio):
+    conexao = pymysql.connect(host='localhost',user='root',password='vp$C1976',db='iaater', autocommit = True)
+    cursor = conexao.cursor()
+    cursor.execute('insert into perfis(perfil, bio) values (%s, %s)', (perfil, bio))
+    idperfil = cursor.lastrowid  
+    cursor.close()
+    conexao.close()
+    return idperfil
+
+def perfilIndexado(perfil):
+    retorno = -1
+    conexao = pymysql.connect(host='localhost',user='root',password='vp$C1976',db='iaater' )
+    cursorUrl = conexao.cursor()
+    cursorUrl.execute('select idperfil from perfis where perfil = %s', perfil)
+    if cursorUrl.rowcount > 0:
+        # print('Url cadastrada')
+        idperfil = cursorUrl.fetchone()[0]
+        cursorPalavra = conexao.cursor()
+        cursorPalavra.execute('select idperfil from palavra_localizacao where idperfil = %s', idperfil)
+        if cursorPalavra.rowcount > 0:
+            # print('Url com palavras')
+            retorno = -2
+        else:
+            # print('Url sem palavras')
+            retorno = idperfil
+            
+        cursorPalavra.close()
+    # else:
+    #     print('Url não cadastrada')
+    
+    cursorUrl.close()
+    conexao.close()
+    
+    return retorno
+
+def separaPalavras(texto):
+    stop = nltk.corpus.stopwords.words('portuguese')
+    stemmer = nltk.stem.RSLPStemmer()
+    splitter = re.compile('\\W')
+    lista_palavras = []
+    lista = [p for p in splitter.split(texto) if p !='']
+    for p in lista:
+        if p.lower() not in stop:
+            if len(p) > 1:
+                lista_palavras.append(stemmer.stem(p).lower())
+    return lista_palavras
+
+def indexador(perfil, bio):
+    indexado = perfilIndexado(perfil)
+    if indexado == -2:
+        print('Perfil já indexado')
+        return
+    elif indexado == -1:
+        idnovoperfil = inserePerfil(perfil, bio)
+    elif indexado > 0:
+        idnovoperfil = indexado
+        
+    print('Indexando '+ perfil)
+    
+    palavras = separaPalavras(bio)
+    for i in range(len(palavras)):
+        palavra = palavras[i]
+        idpalavra = palavraIndexada(palavra)
+        if idpalavra == -1:
+            idpalavra = inserePalavra(palavra)
+        inserePalavraLocalizacao(idnovoperfil, idpalavra, i)
+
+        
+def crawl():
+    df = pd.read_csv("dataset.csv", sep=",", encoding="utf8")
+    for x in range(0, len(df['bio'])):
+        print(df['user'][x], df['bio'][x])
+        indexador(df['user'][x], df['bio'][x])
+        
+crawl()
---
+++ ia.py
@@ -0,0 +1,90 @@
+import pymysql
+import nltk
+from flask import Flask, jsonify, request
+import jsonpickle
+
+def frequenciaScore(linhas):
+    contagem = dict([linha[0], 0] for linha in linhas)
+    for linha in linhas:
+        # print(linha)
+        contagem[linha[0]] += 1
+    return contagem
+
+def pesquisa(consulta):
+    linhas, palavrasid = buscaMaisPalavras(consulta)
+    
+    scores = frequenciaScore(linhas)
+    scoresordenado = sorted([(score, perfil) for (perfil, score) in scores.items()], reverse = 1)
+    perfisLocalizados = set()
+    for(score, idperfil) in scoresordenado:
+        perfisLocalizados.add(getPerfil(idperfil))
+    return perfisLocalizados
+        
+        
+def getPerfil(idperfil):
+    retorno = ''
+    conexao = pymysql.connect(host='localhost',user='root',password='ater',db='iaater', autocommit = True)
+    cursor = conexao.cursor()
+    cursor.execute('select perfil from perfis where idperfil = %s', idperfil)
+    if cursor.rowcount > 0:
+        retorno = cursor.fetchone()[0]
+    
+    cursor.close()
+    conexao.close()
+    return retorno
+
+# getPerfil(3)
+
+def getIdPalavra(palavra):
+    retorno = -1
+    stemmer = nltk.stem.RSLPStemmer()
+    conexao = pymysql.connect(host='localhost',user='root',password='ater',db='iaater', autocommit = True)
+    cursor = conexao.cursor()
+    cursor.execute('select idpalavra from palavras where palavra = %s', stemmer.stem(palavra))
+    if cursor.rowcount > 0:
+        retorno = cursor.fetchone()[0]
+    cursor.close()
+    conexao.close()
+    return retorno
+
+# getIdPalavra('programação')
+
+def buscaMaisPalavras(consulta):
+    listaCampos = 'p1.idperfil'
+    listaTabelas = ''
+    listaClausulas = ''
+    palavrasid = []
+    
+    palavras = consulta.split(' ')
+    numeroTabela = 1
+    for palavra in palavras:
+        idpalavra = getIdPalavra(palavra)
+        if idpalavra > 0:
+            palavrasid.append(idpalavra)
+            if numeroTabela > 1:
+                listaTabelas +=', '
+                listaClausulas += ' and '
+                listaClausulas += 'p%d.idperfil = p%d.idperfil and '%(numeroTabela -1, numeroTabela)
+            listaCampos += ', p%d.localizacao' % numeroTabela
+            listaTabelas += ' palavra_localizacao p%d' % numeroTabela
+            listaClausulas += 'p%d.idpalavra = %d' % (numeroTabela, idpalavra)
+            numeroTabela += 1
+    consultacompleta = 'select %s from %s where %s' % (listaCampos, listaTabelas, listaClausulas)
+    conexao = pymysql.connect(host='localhost',user='root',password='ater',db='iaater', autocommit = True)
+    cursor = conexao.cursor()
+    cursor.execute(consultacompleta)
+    linhas = [linha for linha in cursor]
+    cursor.close()
+    conexao.close()
+    return linhas, palavrasid
+
+perfis = pesquisa('brasil')
+# jsonperfis = jsonpickle.encode(perfis)
+
+# ia = Flask(__name__)
+
+# @ia.route('/apiPesquisa')
+# def apiPesquisa():
+#     return jsonify(jsonperfis)
+
+# ia.run(port=5000, host='localhost', debug = True,use_reloader=False)
